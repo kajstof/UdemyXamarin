37f1a363899d8e3a74b4e0575dc0677ede114135
Krzysztof Krysiak <krysiak86@gmail.com>
2017-09-26 15:11:19 +0200

    Code formatting fix

--- UdemyXamarin/UdemyXamarin/ListViewPage.xaml.cs
+++ UdemyXamarin/UdemyXamarin/ListViewPage.xaml.cs
@@ -20,12 +20,12 @@ namespace UdemyXamarin
             {
                 new ContactGroup("K-key", "K")
                 {
-                    new Contact {Name="Kajstof", ImageUrl="http://lorempixel.com/100/100/people/1" },
-                    new Contact {Name="Krzyś", ImageUrl="http://lorempixel.com/100/100/people/2", Status="Hey, let's talk!" }
+                    new Contact { Name = "Kajstof", ImageUrl = "http://lorempixel.com/100/100/people/1" },
+                    new Contact { Name = "Krzyś", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hey, let's talk!" }
                 },
                 new ContactGroup("L-key", "L")
                 {
-                    new Contact {Name="Lemming", ImageUrl="http://lorempixel.com/100/100/people/3", Status="Hey, let's talk!" }
+                    new Contact { Name = "Lemming", ImageUrl = "http://lorempixel.com/100/100/people/3", Status = "Hey, let's talk again!" }
                 }
             };
             listView.ItemsSource = names;
